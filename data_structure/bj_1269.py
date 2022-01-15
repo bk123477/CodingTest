8aba7a71a8e1a4d1101b27d06ec93ee269c3e715
bk123477 <bk123477@naver.com>
2022-01-15 16:02:40 +0900

    Baekjoon data_structure algorithm

---
+++ data_structure/bj_1269.py
@@ -0,0 +1,27 @@
+import sys
+n, m = map(int, sys.stdin.readline().rstrip().split())
+a = list(map(int, sys.stdin.readline().rstrip().split()))
+b = list(map(int, sys.stdin.readline().rstrip().split()))
+a.sort()
+b.sort()
+n_a, n_b = 0, 0
+a_index, b_index = 0, 0
+while True:
+    if a_index == n or b_index == m:
+        break
+    else:
+        if a[a_index] < b[b_index]:
+            n_a += 1
+            a_index += 1
+        elif a[a_index] > b[b_index]:
+            n_b += 1
+            b_index += 1
+        else:
+            a_index += 1
+            b_index += 1
+if a[n-1] > b[m-1]:
+    n_a += n - a_index
+elif a[n-1] < b[m-1]:
+    n_b += m - b_index
+
+print(n_a + n_b)
