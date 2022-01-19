530c2d192889acece7ceb5655ee6af5a2d781d2c
bk123477 <bk123477@naver.com>
2022-01-19 16:05:04 +0900

    Baekjoon data_structure algorithm

--- data_structure/bj_1935.py
+++ data_structure/bj_1935.py
@@ -4,12 +4,12 @@ from collections import deque
 n = int(sys.stdin.readline().rstrip()) # 숫자 개수
 operation = list(sys.stdin.readline().rstrip())
 number = {}
+my_stack = deque()
 for i in range(n):
     number[chr(65+i)] = int(sys.stdin.readline().rstrip())
-my_stack = deque()
 
 for i in operation:
-    if 'A' <= i <= 'Z': # dictionary에서 해당key에서 value 뽑아오기
+    if 'A' <= i <= 'Z': # dictionary 해당key에서 value 뽑아오기
         my_stack.append(number[i])
     else:
         two = my_stack.pop()
