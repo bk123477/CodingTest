71eeb75dc38c4210300e08d063ddfeb8f5acd1d5
bk123477 <bk123477@naver.com>
2022-01-23 15:57:39 +0900

    Baekjoon greedy algorihtm

---
+++ greedy/bj_1105.py
@@ -0,0 +1,15 @@
+import sys
+
+l, r = map(str, sys.stdin.readline().rstrip().split())
+
+if len(l) != len(r):
+    print(0)
+else:
+    cnt = 0
+    for i in range(len(l)):
+        if l[i] == r[i]:
+            if l[i] == '8':
+                cnt+=1
+        else:
+            break
+    print(cnt)
