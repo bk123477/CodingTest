848214de054a00c729e2f0687d1edc6f6ef20cac
bk123477 <bk123477@naver.com>
2022-02-02 13:53:50 +0900

    Baekjoon graph algorithm

---
+++ graph/bj_1012.py
@@ -0,0 +1,32 @@
+import sys
+input = sys.stdin.readline
+sys.setrecursionlimit(6250000)
+
+def dfs(graph,y,x, n, m):
+    if y<0 or x<0 or y>=n or x>=m:
+        return False
+    if graph[y][x] == 1:
+        graph[y][x] = 0
+        dfs(graph, y-1, x, n, m)
+        dfs(graph, y+1, x, n, m)
+        dfs(graph, y, x-1, n, m)
+        dfs(graph, y, x+1, n, m)
+        return True
+    return False
+
+t = int(input().rstrip())
+result = []
+for _ in range(t):
+    n, m, k = map(int, input().rstrip().split())
+    graph = [[0 for _ in range(m)] for _ in range(n)]
+    count = 0
+    for _ in range(k):
+        yloc, xloc = map(int, input().rstrip().split())
+        graph[yloc][xloc] = 1
+    for i in range(n):
+        for j in range(m):
+            if dfs(graph,i,j,n,m):
+                count +=1
+    result.append(count)
+for v in result:
+    print(v)
