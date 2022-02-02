d7da1b51e51b0d6e3db89ee73fa35e06f0b1ef1a
bk123477 <bk123477@naver.com>
2022-02-02 14:02:49 +0900

    Baekjoon graph algorithm

--- graph/bj_1012.py
+++ graph/bj_1012.py
@@ -1,12 +1,12 @@
 import sys
 input = sys.stdin.readline
-sys.setrecursionlimit(6250000)
+sys.setrecursionlimit(6250000) # RecursionError 방지
 
 def dfs(graph,y,x, n, m):
-    if y<0 or x<0 or y>=n or x>=m:
+    if y<0 or x<0 or y>=n or x>=m: # graph 범위 벗어나면 False 리턴
         return False
-    if graph[y][x] == 1:
-        graph[y][x] = 0
+    if graph[y][x] == 1: # 배추의 상하좌우 탐색
+        graph[y][x] = 0 # 방문 처리
         dfs(graph, y-1, x, n, m)
         dfs(graph, y+1, x, n, m)
         dfs(graph, y, x-1, n, m)
@@ -20,7 +20,7 @@ for _ in range(t):
     n, m, k = map(int, input().rstrip().split())
     graph = [[0 for _ in range(m)] for _ in range(n)]
     count = 0
-    for _ in range(k):
+    for _ in range(k): # 배추의 위치를 입력
         yloc, xloc = map(int, input().rstrip().split())
         graph[yloc][xloc] = 1
     for i in range(n):
