a54f5d18748c29f9b580c8b019ce66ac6b089304
bk123477 <bk123477@naver.com>
2022-02-13 12:58:18 +0900

    Baekjoon graph algorithm

--- graph/bj_5014.py
+++ graph/bj_5014.py
@@ -3,19 +3,19 @@ from collections import deque
 input = sys.stdin.readline
 
 f, s, g, u, d = map(int, input().split())
-move = [u, -d]
+move = [u, -d] # bfs 탐색하기 위한 리스트
 
 def bfs():
-    queue = deque([s])
+    queue = deque([s]) # 시작층은 s
     floor = [-1] * (f+1)
     floor[s] = 0
     while queue:
         current = queue.popleft()
-        if current == g:
+        if current == g: # 현재 위치가 g라면 while문 탈출
             break
         for i in move:
             next = current + i
-            if 0<next<=f and floor[next] == -1:
+            if 0<next<=f and floor[next] == -1: # 범위 안에 있고, 아직 탐색하지 않은 층
                 floor[next] = floor[current] + 1
                 queue.append(next)
     if floor[g] == -1:
